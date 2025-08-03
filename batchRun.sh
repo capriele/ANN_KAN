ef2e15a446afb6781d04a031f55a2ae51bf8b95f
Alberto Petrucci <petrucci.alberto@gmail.com>
2025-08-03 23:30:04 +0200

    Added new results

--- batchRun.sh
+++ batchRun.sh
@@ -5,5 +5,5 @@ touch  "results/$1.txt"
 echo $2 $3 $4 $5 $6 $7 $8
 for i in {1..10}
 do
-	python -W ignore main.py $i $2 $3 $4 $5 $6 $7 $8 | egrep "para|systemSelector|fit|NRMSE|f1|elapsed|evaluating|encoder|decoder|bridge" | tee -a  "results/$1.txt"
+	python3 -W ignore main.py $i $2 $3 $4 $5 $6 $7 $8 | tee >(egrep "para|systemSelector|fit|NRMSE|f1|elapsed|evaluating|encoder|decoder|bridge" >> "results/$1.txt")
 done
--- hpc_cluster_run.sh
+++ hpc_cluster_run.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
-#SBATCH --job-name=ann_kan
+#SBATCH --job-name=ann_kan_2
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
 #SBATCH --time=23:00:00
-#SBATCH --gres=gpu:a100:8
 
-module purge
-module load python/3.10.8  # Load the appropriate Python module if on a system with module system
+# User specific aliases and functions
+export HOME="/home/petruccia"
+export PATH="$HOME/bin/bin:$HOME/bin:$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
-# Create and activate a virtualenv in the job's scratch directory
-VENV_DIR="$SLURM_TMPDIR/myenv"
-python -m venv $VENV_DIR
-source $VENV_DIR/bin/activate
+export PATH="$HOME/local/bin:$PATH"
+export PATH="$HOME/local/python3.11/bin/:$PATH"
 
-# cloning the repo
-git clone https://github.com/capriele/ANN_KAN.git ann_kan
-cd ann_kan
+# GCC 15.1.0
+export PATH="$HOME/local/gcc-15.1.0/bin:$PATH"
+export LD_LIBRARY_PATH="$HOME/local/gcc-15.1.0/lib64:$LD_LIBRARY_PATH"
+export LIBRARY_PATH="$HOME/local/gcc-15.1.0/lib64:$LIBRARY_PATH"
+export C_INCLUDE_PATH="$HOME/local/gcc-15.1.0/include:$C_INCLUDE_PATH"
+export CPLUS_INCLUDE_PATH="$HOME/local/gcc-15.1.0/include:$CPLUS_INCLUDE_PATH"
 
-# Upgrade pip and install requirements
-pip install --upgrade pip
-pip install -r requirements.txt
+# TMUX
+export PATH=$HOME/local/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/local/python3.11/lib:$HOME/local/lib64:$HOME/local/lib/pkgconfig:$HOME/local/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=$HOME/local/lib/pkgconfig:$PKG_CONFIG_PATH
+export CPATH=$HOME/local/include:$CPATH
+export LIBRARY_PATH=$HOME/local/python3.11/lib:$HOME/local/lib64:$HOME/local/lib/pkgconfig:$HOME/local/lib:$LIBRARY_PATH
+
+# cd $HOME/local/src
+# cd Python-3.11.6
+
+# ./configure --prefix=$HOME/local/python3.11 --enable-shared --enable-optimizations CPPFLAGS="-I$HOME/local/include" LDFLAGS="-L$HOME/local/lib -L$HOME/local/lib64" PKG_CONFIG_PATH="$HOME/local/lib/pkgconfig"
+
+# make -j
+# make install
+
+# Try with minimal build
+#PYTHON_CONFIGURE_OPTS="--enable-shared --with-ensurepip=install" pyenv install 3.11.6
+#pyenv virtualenv 3.11.6 test_3_11
+#pyenv activate test_3_11
 
 # Run your python task (replace script.py with your script name)
-./batchRun.sh NLTankNLF5Affine 5 1 1 6 10 1 0
+#cd $HOME/pykan/ann_kan/
+#pip install -r requirements.txt
+./batchRun.sh $1 $2 $3 $4 $5 $6 $7 $8
 
-# (The environment will be deactivated and cleaned up at job end)
+#pyenv deactivate
