b629164b6696c22c123930b9d977f8247e2b5c45
Alberto Petrucci <petrucci.alberto@gmail.com>
2025-08-10 11:56:14 +0200

    Added ANN KAN based models

--- batchRun.sh
+++ batchRun.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-rm  "results/$1.txt"
-touch  "results/$1.txt"
-echo $2 $3 $4 $5 $6 $7 $8
+if [ "$9" = "1" ]; then
+    filename="results/${1}_KAN.txt"
+else
+    filename="results/${1}.txt"
+fi
+
+rm -f "$filename"
+touch "$filename"
+
+echo $2 $3 $4 $5 $6 $7 $8 $9
 for i in {1..10}
 do
-	python3 -W ignore main.py $i $2 $3 $4 $5 $6 $7 $8 | tee >(egrep "para|systemSelector|fit|NRMSE|f1|elapsed|evaluating|encoder|decoder|bridge" >> "results/$1.txt")
+	python3 -W ignore main.py $i $2 $3 $4 $5 $6 $7 $8 $9 | tee >(egrep "para|systemSelector|fit|NRMSE|f1|elapsed|evaluating|encoder|decoder|bridge" >> "$filename")
 done
